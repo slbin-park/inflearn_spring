1bda0529faf0ce81e1f0505399465227f6bcfa84
Chu-Jiwoo <smpts00@naver.com>
2022-07-03 21:44:32 +0900

    Add study_spring

---
+++ hello-spring/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -0,0 +1,13 @@
+package hello.hellospring;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.service.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfig {
+
+}
--- hello-spring/hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ hello-spring/hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -1,9 +1,11 @@
 package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
+import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
+@Repository
 public class MemoryMemberRepository implements  MemberRepository{
 
     private static Map<Long,Member> store = new HashMap<>();
@@ -12,7 +14,7 @@ public class MemoryMemberRepository implements  MemberRepository{
     @Override
     public Member save(Member member) {
         member.setId(++sequence);
-        store.put(member.getId(),member);
+        store.put(member.getId(), member);
         return member;
     }
 
