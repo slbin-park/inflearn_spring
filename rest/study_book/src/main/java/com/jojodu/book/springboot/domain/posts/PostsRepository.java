dc7ceb197d473bf3c0667f740e5da84ae58bf5f4
Chu-Jiwoo <smpts00@naver.com>
2022-07-10 21:37:22 +0900

    Add Google & Naver login

--- rest/study_book/src/main/java/com/jojodu/book/springboot/domain/posts/PostsRepository.java
+++ rest/study_book/src/main/java/com/jojodu/book/springboot/domain/posts/PostsRepository.java
@@ -1,10 +1,16 @@
 package com.jojodu.book.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 // JpaRepository <Entity 클래스 , PK 타입>
 // 를 상속하면 기본적인 CRUD 메서드가 자동으로 생성이 된다.
 // @Repository 를 추가할 필요가 없음
 // 그리고 Entity 클래스와 기본 Entity Repository는 함께 위치해야함
 public interface PostsRepository extends JpaRepository<Posts,Long> {
+
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDESC();
 }
--- rest/study_book/src/main/java/com/jojodu/book/springboot/service/posts/PostsService.java
+++ rest/study_book/src/main/java/com/jojodu/book/springboot/service/posts/PostsService.java
@@ -2,13 +2,16 @@ package com.jojodu.book.springboot.service.posts;
 
 import com.jojodu.book.springboot.domain.posts.Posts;
 import com.jojodu.book.springboot.domain.posts.PostsRepository;
+import com.jojodu.book.springboot.web.dto.PostsListResponseDto;
 import com.jojodu.book.springboot.web.dto.PostsResponseDto;
 import com.jojodu.book.springboot.web.dto.PostsSaveRequestDto;
 import com.jojodu.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import javax.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -29,10 +32,24 @@ public class PostsService {
         return id;
     }
 
+    @Transactional(readOnly = true)
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = "+ id));
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
         return new PostsResponseDto(entity);
     }
 
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDESC().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id= " + id));
+        postsRepository.delete(posts);
+    }
 }
